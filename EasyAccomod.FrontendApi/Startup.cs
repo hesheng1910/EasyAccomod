b8ed5fd497508cff47c790b946adfbe2ee631950
Cuong020900 <trancuong.02092000@gmail.com>
2020-12-26 16:29:25 +0700

    fix mot so loi

--- EasyAccomod.FrontendApi/Startup.cs
+++ EasyAccomod.FrontendApi/Startup.cs
@@ -47,7 +47,7 @@ namespace EasyAccomod.FrontendApi
                                   builder =>
                                   {
                                       builder.WithOrigins("http://localhost:9999",
-                                                          "http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+                                                          "http://localhost:3000").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
                                   });
             });
 
