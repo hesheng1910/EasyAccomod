fa5364d38a094fc193da8cd0ef525bd69c20b60b
hesheng1910 <56876037+hesheng1910@users.noreply.github.com>
2020-12-26 10:56:44 +0700

    Statistic

---
+++ EasyAccomod.Core/Model/DateViewPost/ViewPostPerDay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyAccomod.Core.Model.DateViewPost
+{
+    public class ViewPostPerDay
+    {
+        public long TotalViewPerDay { get; set; }
+        public DateTime Day { get; set; }
+    }
+}
--- EasyAccomod.Core/Services/DateViewPosts/DateViewPostService.cs
+++ EasyAccomod.Core/Services/DateViewPosts/DateViewPostService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AGID.Core.Exceptions;
+using EasyAccomod.Core.EF;
+using EasyAccomod.Core.Model.DateViewPost;
+using EasyAccomod.Core.Services.Posts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +11,26 @@ namespace EasyAccomod.Core.Services.DateViewPosts
 {
     public class DateViewPostService : IDateViewPostService
     {
+        private readonly EasyAccDbContext easyAccDbContext;
 
+        public DateViewPostService(EasyAccDbContext easyAccDbContext)
+        {
+            this.easyAccDbContext = easyAccDbContext;
+        }
+
+        public List<ViewPostPerDay> GetViewPostPerDays(long accessId,long postId)
+        {
+            var post = easyAccDbContext.Posts.Where(x => x.PostId == postId && x.PostStatus == Enums.PostStatusEnum.Accepted);
+            if (post == null) throw new ServiceException("Post khong ton tai");
+            List<ViewPostPerDay> viewPostPerDays = new List<ViewPostPerDay>()
+            { 
+                new ViewPostPerDay(){ Day = new DateTime(2020,12,26), TotalViewPerDay = 123 - 10*postId},
+                new ViewPostPerDay(){ Day = new DateTime(2020,12,27), TotalViewPerDay = 143 + 10*postId},
+                new ViewPostPerDay(){ Day = new DateTime(2020,12,28), TotalViewPerDay = 312 - 10*postId},
+                new ViewPostPerDay(){ Day = new DateTime(2020,12,29), TotalViewPerDay = 325 - 10*postId},
+                new ViewPostPerDay(){ Day = new DateTime(2020,12,30), TotalViewPerDay = 125 + 10*postId}
+            };
+            return viewPostPerDays;
+        }
     }
 }
